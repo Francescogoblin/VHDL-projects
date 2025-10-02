fa7c31af4343c1f15e99847f436696e665cb2a93
Francescogoblin <166855620+Francescogoblin@users.noreply.github.com>
2025-10-02 16:15:27 +0200

    Create alu_logic.vhd

---
+++ alu_logic.vhd
@@ -0,0 +1,33 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity ALU is
+
+    Port (
+    
+    op_sel : in std_logic_vector(1 DOWNTO 0);
+    a : in signed(7 DOWNTO 0);
+    b : in signed(7 DOWNTO 0);
+    c : out signed(7 DOWNTO 0)
+
+          );
+
+end ALU;
+
+architecture Behavioral of ALU is
+
+      signal add, sub : signed(7 DOWNTO 0);
+      signal mul : signed(15 DOWNTO 0);
+      
+begin
+
+    add <= a + b;
+    sub <= a - b;
+    mul <= a * b;
+    
+    with op_sel select c <= add when "00",
+                            sub when "01",
+                            mul(c'RANGE) when "10",
+                            (Others => '0') when "11";
+end Behavioral;
