c018f7cbe34c3f502f4facda0492ea797dd31250
Francesco Donini <166855620+Francescogoblin@users.noreply.github.com>
2025-10-05 10:36:48 +0200

    Modifica identazione

--- 9-10-2025/half_adder.vhd
+++ 9-10-2025/half_adder.vhd
@@ -2,17 +2,20 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 
 entity half_adder is
+  
   Port (
-     a : in std_logic; 
-     b : in std_logic;
-     c : out std_logic
-     sum : out std_logic;
+     a   : in std_logic; 
+     b   : in  std_logic;
+     c   : out std_logic;
+     sum : out std_logic
    );
+  
 end half_adder;
 
 architecture Behavioral of half_adder is
 
 begin
+  
     c <= a and b;
     sum <= a xor b; 
 
