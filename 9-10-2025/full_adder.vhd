5937acbd0c36bb4dc6db323c5ca2151a6a094758
Francesco Donini <166855620+Francescogoblin@users.noreply.github.com>
2025-10-05 10:36:55 +0200

    Modifica identazione

--- 9-10-2025/full_adder.vhd
+++ 9-10-2025/full_adder.vhd
@@ -2,12 +2,17 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity full_adder is
+  
   Port (
+    
      a_fa      : in    std_logic; 
      b_fa      : in    std_logic;
      c_in_fa   : in    std_logic;
      sum_fa    : out   std_logic;
      c_out_fa  : out   std_logic 
+
+  );
+    
 end full_adder;
 
 architecture Behavioral of full_adder is
@@ -16,22 +21,27 @@ architecture Behavioral of full_adder is
   signal sig_sum     : std_logic;
 
   component half_adder is
+    
     Port (
        a       : in  std_logic;
        b       : in  std_logic;
        sum     : out std_logic;
        c_out   : out std_logic
      );
+    
   end component;
 
 begin
+  
   ha_inst1: half_adder
+    
     port map(
       a => a_fa,
       b => b_fa,
       sum => sig_sum,
       c_out => sig_c_out1
     );
+    
     ha_inst2: half_adder
       port map(
         a => sig_sum,
