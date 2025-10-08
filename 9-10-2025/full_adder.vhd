f743fffc2aa741115133148e4a079b67a07308a0
Francesco Donini <166855620+Francescogoblin@users.noreply.github.com>
2025-10-08 19:05:41 +0200

    Update full_adder.vhd

--- 9-10-2025/full_adder.vhd
+++ 9-10-2025/full_adder.vhd
@@ -20,27 +20,27 @@ architecture Behavioral of full_adder is
   signal sig_c_out2  : std_logic;
   signal sig_sum     : std_logic;
 
-  component half_adder is --ecco perchè abbiamo prima creato un half adder, ora lo utilizziamo come component
+    component half_adder is --ecco perchè abbiamo prima creato un half adder, ora lo utilizziamo come component
     
     Port (
        a       : in  std_logic;
        b       : in  std_logic;
        sum     : out std_logic;
-       c_out   : out std_logic
+       c       : out std_logic
      );
     
   end component;
 
 begin
   
-  ha_inst1 : half_adder --creo un'istanza che si occupa diceseguire la somma dei due numeri originali e 
+  ha_inst1 : half_adder  --creo un'istanza che si occupa diceseguire la somma dei due numeri originali e 
 -- mette la somma in sig_sum, mentre il riporto in sig_c_out1
     
     port map(
       a     =>   a_fa,
       b     =>   b_fa,
       sum   =>   sig_sum,
-      c_out =>   sig_c_out1
+      c     =>   sig_c_out1
     );
     
   ha_inst2: half_adder --creo una seconda istanza che somma la somma dei due numeri di prima (sign_sum) ed il 
@@ -50,9 +50,10 @@ begin
       a     =>   sig_sum,
       b     =>   c_in_fa,
       sum   =>   sum_fa,
-      c_out =>   sig_c_out2
+      c     =>   sig_c_out2
       );
 
       c_out_fa <= sig_c_out1 or sig_c_out2; --il riporto di uscita è un or dei due riporti
 
 end Behavioral;
+
