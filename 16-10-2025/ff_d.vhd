2bb43951401ba01866def9dac72df8f86318ca77
Francesco Donini <166855620+Francescogoblin@users.noreply.github.com>
2025-10-16 10:31:21 +0200

    Refactor D flip-flop entity to use reset_ff and clk_ff

--- 16-10-2025/ff_d.vhd
+++ 16-10-2025/ff_d.vhd
@@ -1,28 +1,22 @@
--- QUESTA ENTITY E' UN BISTABILE DI TIPO D
-
 library IEEE;
 	use IEEE.STD_LOGIC_1164.ALL;
 
-
 entity ff_d is
 	Port(
-		reset	: in std_logic;
-		clk		: in std_logic;
+		reset_ff	: in  std_logic;
+		clk_ff		: in  std_logic;
 
-		d 	    : in std_logic;
-		q 	    : out std_logic
-	);	
+		d 	        : in  std_logic;
+		q 	        : out std_logic
+	);
 end ff_d;
 
-	
 architecture Behavioral of ff_d is
-	
-	signal q_int   :   std_logic := 'U'; -- Creo un signal di valore "U"
-
+	signal q_int : std_logic := 'U';
 begin
 
-	q_int	<= 	'0' when reset = '1'      else         -- il segnale è 0 se il reset è alto
-				d 	when rising_edge(clk) else q_int;  -- il segnale è uguale a d ogni volta che il clock sale , sennò resta uguale a se stesso
+	q_int	<= 	'0' when reset_ff = '1' else
+				d 	when rising_edge(clk_ff) else q_int;
 
 	q <= q_int;
 	
