8e0d552009f0d12a904130976f8daf63be42fbf1
Francesco Donini <166855620+Francescogoblin@users.noreply.github.com>
2025-10-24 16:05:59 +0200

    Riscrittura del codice con molti commenti e con una condizione in più per migliorare un possibile bug

--- 16-10-2025/ShiftRegisterSIPO_1st_edition.vhd
+++ 16-10-2025/ShiftRegisterSIPO_1st_edition.vhd
@@ -1,63 +1,66 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
+
 entity ShiftRegisterSIPO is
-	
-Generic(
-SR_WIDTH :integer:= 4
-); 
-Port(
-reset    : in  std_logic;
-clk      : in  std_logic;
-data_in  : in  std_logic;
-data_out : out std_logic_vector(SR_WIDTH-1 DOWNTO 0)
-);     
+    Generic(
+        SR_WIDTH : integer := 4
+    );
+    
+    Port(
+        reset       : in std_logic;
+        clk         : in std_logic;
+        
+        data_in     : in std_logic;
+        data_out    : out std_logic_vector(SR_WIDTH-1 DOWNTO 0)
+    );
 end ShiftRegisterSIPO;
 
 
 architecture Behavioral of ShiftRegisterSIPO is
-	
-	component ff_d is
-    	Port(
-			reset_ff	: in std_logic;
-			clk_ff    : in std_logic;
-			d 		: in std_logic;
-			q 		: out std_logic
-		);  
-	end component;
-	   
-	signal n : std_logic_vector(3 DOWNTO 0);
+
+        component ff_d is
+            Port (
+            
+                d        : in std_logic;
+                q        : out std_logic;
+                
+                clk      : in std_logic;
+                reset    : in std_logic
+                
+            );
+        end component;
+        
+        signal n : std_logic_vector(SR_WIDTH-1 DOWNTO 0);      --   CREO UN SEGNALE N LUNGO 4
 
 begin
-	if SR_WIDTH = 0 then
-		
-    	report "Errore: STD è uguale a 1" severity error;
-	
-	elsif SR_WIDTH > 0 then
-	
-	ff_d_inst_0 : ff_d                                                          --INSTANZA INIZIALE
-    	Port Map (
-        	reset_ff => reset,
-            clk_ff   => clk,
-        	d        => data_in,
-       	    q        => n(0)
-        );
-            
-   	LOOP_GEN  : for I in 0 to SR_WIDTH-2 generate                                 --INIZIO DEL LOOP
-        
-    ff_d_inst_i : ff_d
-		Port Map (
-        	reset_ff => reset,
-            clk_ff   => clk,
-        	d        => n(I),
-        	q        => n(I+1)
-        );
-               
-    end generate;
-    	
-	data_out <= n;
 
-	end if;
+    COND_OK : if SR_WIDTH > 1 generate  -- QUANDO LA LUNGHEZZA DEL REGISTRO DA CREARE E' MAGGIORE DI 1 ( AD ESEMPIO SR_WIDTH = 4)
+    
+            ff_d_inst_0 : ff_d          -- CREAZIONE DEL REGISTRO PIU' A SINSITRA
+            Port Map (
+                d => data_in,    -- A D ASSEGNO DATA IN
+                q => n(0),       -- L'USCITA DEL PRIMO REGISTRO E' IL PRIMO BIT DEL SEGNALE
                 
-end Behavioral;
-
+                clk => clk,
+                reset => reset
+            );
+    
+        LOOP_int : for I in 0 to SR_WIDTH-2 generate     -- DOPODICHE' GENERO I RESTANTI 3 FLIP FLOP ( CON I=0, I=1, I=2)
+        
+            ff_d_inst : ff_d
+            Port Map (
+                d => n(I),        --  A D ASSEGNO N(0) CHE ERA L'USCITA DI QUELLO PRIMA
+                q => n(I+1),      --  A Q ASSEGNO N(1) 
+                
+                clk => clk,
+                reset => reset
+            );
+            
+            
+        end generate;
+        
+        data_out <= n;  -- SVUOTO TUTTO IL VETTORE N IN DATA OUT
+    
+     end generate;
+   
