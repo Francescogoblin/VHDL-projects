346d96e5e8077422e27045fe4183e428bb998dfa
Francesco Donini <166855620+Francescogoblin@users.noreply.github.com>
2025-10-24 16:23:26 +0200

    Add UpDownSyncCounter implementation
    Implemented an UpDownSyncCounter that increments or decrements based on input signals.

--- 24-10-2025/UpDownSyncCounter.vhd
+++ 24-10-2025/UpDownSyncCounter.vhd
@@ -1,1 +1,38 @@
---codice
+-- IMPLEMENTAZIONE DI UN CONTATORE CHE SOMMA O SOTTRAE 1 AL VARIARE DEI SUOI DUE INGRESSI: SE SONO DIVERSI SOMMA O SOTTRAE 1
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+
+entity UpDownSyncCounter is
+    Generic(
+        COUNT_WIDTH : integer := 4
+        );
+        
+        Port(
+        reset       : in std_logic;
+        clk         : in std_logic;
+        inc_count   : in std_logic;
+        dec_count   : in std_logic;
+        count       : out std_logic_vector (COUNT_WIDTH-1 DOWNTO 0) --SIGNED
+        );  
+end UpDownSyncCounter;
+
+    
+architecture Behavioral of UpDownSyncCounter is
+
+component ff_d is
+        Port(
+		reset	: in std_logic;
+		clk		: in std_logic;
+
+		d 	    :	in std_logic;
+		q 	    : out std_logic
+	);
+	end component;
+
+
+begin
+
+
+end Behavioral;
