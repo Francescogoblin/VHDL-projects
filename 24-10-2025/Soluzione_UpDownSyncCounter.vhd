1460279230b8e0031a89d5898ab912d6b1ecb4aa
Francesco Donini <166855620+Francescogoblin@users.noreply.github.com>
2025-10-31 13:39:31 +0100

    Update Soluzione_UpDownSyncCounter.vhd

--- 24-10-2025/Soluzione_UpDownSyncCounter.vhd
+++ 24-10-2025/Soluzione_UpDownSyncCounter.vhd
@@ -1,1 +1,31 @@
-library 
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+
+entity UpDownSyncCounter is
+    Generic(
+        COUNT_WIDTH : integer := 4
+        );
+
+        Port(
+        reset       : in std_logic;
+        clk         : in std_logic;
+        inc_count   : in std_logic;
+        dec_count   : in std_logic;
+        count       : out std_logic_vector (COUNT_WIDTH-1 DOWNTO 0) --SIGNED
+        );  
+end UpDownSyncCounter;
+
+
+architecture Behavioral of UpDownSyncCounter is
+
+component ff_d is
+        Port(
+                reset        : in std_logic;
+                clk                : in std_logic;
+
+                d             : in std_logic;
+                q             : out std_logic
+        );
+        end component; 
