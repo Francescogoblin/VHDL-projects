b8a78aa50cab64e633fdd0b919fd2249788ac128
Francesco Donini <166855620+Francescogoblin@users.noreply.github.com>
2025-11-03 17:18:36 +0100

    Update Soluzione_UpDownSyncCounter.vhd

--- 24-10-2025/Soluzione_UpDownSyncCounter.vhd
+++ 24-10-2025/Soluzione_UpDownSyncCounter.vhd
@@ -23,9 +23,9 @@ architecture Behavioral of UpDownSyncCounter is
 component ff_d is
         Port(
                 reset        : in std_logic;
-                clk                : in std_logic;
+                clk          : in std_logic;
 
-                d             : in std_logic;
-                q             : out std_logic
+                d            : in std_logic;
+                q            : out std_logic
         );
         end component; 
