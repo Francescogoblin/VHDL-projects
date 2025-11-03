84c19046159fb06139865b461d7a66bb2a7d3b44
Francesco Donini <166855620+Francescogoblin@users.noreply.github.com>
2025-11-03 18:16:49 +0100

    Update Soluzione_UpDownSyncCounter.vhd

--- 24-10-2025/Soluzione_UpDownSyncCounter.vhd
+++ 24-10-2025/Soluzione_UpDownSyncCounter.vhd
@@ -1,6 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;       --PER POTER USARE LA LIBRERIA NUMERIC
 
 
 entity UpDownSyncCounter is
@@ -17,7 +17,10 @@ entity UpDownSyncCounter is
         );  
 end UpDownSyncCounter;
 
-entity my_register is
+
+architecture Behavioral of UpDownSyncCounter is
+
+component my_register is
         Port(
                 reset        : in std_logic;
                 clk          : in std_logic;
@@ -27,30 +30,14 @@ entity my_register is
         );
         end component;
 
+signal addendo : signed (0 TO 1);                   -- LA SOMMA, PER DUE TIPI SIGNED, È DEFINITA ANCHE PER ADDENDI CHE HANNO LUNGHEZZA DIFFERENTE ED IL RIS È IL MAX
+signal reg_out : std_logic_vector(count'RANGE);   
+signal reg_in  : std_logic_vector(count'RANGE);
 
-architecture Behavioral of UpDownSyncCounter is
+begin
 
-component ff_d is
-        Port(
-                reset        : in std_logic;
-                clk          : in std_logic;
-
-                d            : in std_logic; 
-                q            : out std_logic
-        );
-end component;
-
-REG_GEN : for I in 0 to D'RANGE generate
-    ff_d_inst : ff_d
-        Port Map( 
-            reset => reset,
-            clk   => clk,
-
-            d     =>D(I),     
-            q     =>Q(I)
-        );
-    end generate
-
+end Behavioral;
+ 
 
 
 
