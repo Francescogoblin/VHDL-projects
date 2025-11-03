46f0c49bf332e90bc1adb7d99fdbd048ccf7b56d
Francesco Donini <166855620+Francescogoblin@users.noreply.github.com>
2025-11-03 17:51:30 +0100

    Update Soluzione_UpDownSyncCounter.vhd

--- 24-10-2025/Soluzione_UpDownSyncCounter.vhd
+++ 24-10-2025/Soluzione_UpDownSyncCounter.vhd
@@ -17,6 +17,16 @@ entity UpDownSyncCounter is
         );  
 end UpDownSyncCounter;
 
+entity my_register is
+        Port(
+                reset        : in std_logic;
+                clk          : in std_logic;
+
+                D            : in std_logic_vector; -- Per la dimensione di D e Q, che sarà la stessa, non possono essere predefinite
+                Q            : out std_logic_vector -- Scrivo std_logic_vector senza scrivere la dimensione, uso un unconstraint array
+        );
+        end component;
+
 
 architecture Behavioral of UpDownSyncCounter is
 
@@ -25,7 +35,24 @@ component ff_d is
                 reset        : in std_logic;
                 clk          : in std_logic;
 
-                d            : in std_logic;
+                d            : in std_logic; 
                 q            : out std_logic
         );
-        end component; 
+end component;
+
+REG_GEN : for I in 0 to D'RANGE generate
+    ff_d_inst : ff_d
+        Port Map( 
+            reset => reset,
+            clk   => clk,
+
+            d     =>D(I),     
+            q     =>Q(I)
+        );
+    end generate
+
+
+
+
+
+    
