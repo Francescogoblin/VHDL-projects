d21a4b9bd036d1d411226d53703566dc88c6e652
Francesco Donini <166855620+Francescogoblin@users.noreply.github.com>
2025-11-03 18:43:07 +0100

    Update Soluzione_UpDownSyncCounter.vhd

--- 24-10-2025/Soluzione_UpDownSyncCounter.vhd
+++ 24-10-2025/Soluzione_UpDownSyncCounter.vhd
@@ -36,6 +36,21 @@ signal reg_in  : std_logic_vector(count'RANGE);
 
 begin
 
+my_register_inst : my_register
+      Port Map (
+          reset  => reset,
+          clk    => clk,
+
+          D      => reg_in,
+          Q      => reg_out;  
+          );
+
+    reg_in <= td_logic_vector (signed(reg_out) + addendo) ;  -- NELLO STD_LOGIC_VECTOR LA SOMMA NON È DEFINITA
+
+    addendo <= to_signed(1 , 2 )  when inc_count = '1' and dec_count = '0' else   --ATTENZIONE! ADDENDO È UN SIGNED MENTRE .1 È UNO SCALARE
+               to signed(-1, 2 )  when inc_count = '1' and dec_count = '0' else   --METTO TO SIGNED ( VALORE , DIMENSIONE) 
+               to signed(0 , 2 ) ;
+
 end Behavioral;
  
 
