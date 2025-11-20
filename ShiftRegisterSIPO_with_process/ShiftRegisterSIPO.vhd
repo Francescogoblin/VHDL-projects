43067146f4c6e1926b6178ecc68e350dbe52ef23
Francesco Donini <166855620+Francescogoblin@users.noreply.github.com>
2025-11-20 18:29:58 +0100

    Update ShiftRegisterSIPO.vhd

--- ShiftRegisterSIPO_with_process/ShiftRegisterSIPO.vhd
+++ ShiftRegisterSIPO_with_process/ShiftRegisterSIPO.vhd
@@ -13,35 +13,33 @@ end ShiftRegisterSIPO;
 architecture Behavioral of ShiftRegisterSIPO is
 
 signal n1 : std_logic;
-signal n2 : std_logic_vector(3 DOWNTO 0);
+signal n2 : std_logic_vector;
 
 begin
-
 
 process (clk, reset)
-begin
+   begin
 
-n1 <= data_in;
+      n1 <= data_in;
 
-for I in range 0 to data_out'RANGE-1 loop
+   for I in range 0 to data_out'RANGE-1 loop
 
-     if reset= '1' then
-         
+         if reset= '1' then
+            n2 <= ( Others => '0');
+         elsif rising_edge(clk) then
+            if I=0 then
+               n2(I) <= n1;
+               n2(I+1) <= n2(I);
+            elsif I/=0 then
+               n2(I+1) <= n2(I);
+            end if;
+         end if;
 
+      data_out(I) <= n2(I)
+            
+   end loop;
 
-
-end loop;
-
-
-
-
-        end process;
-
-
-
-
-
-
+end process;
 
 
 
