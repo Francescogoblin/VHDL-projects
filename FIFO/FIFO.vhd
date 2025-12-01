be67c82cd238f8cbadebc787e38ae4992f254949
Francesco Donini <166855620+Francescogoblin@users.noreply.github.com>
2025-12-01 11:22:41 +0100

    Update FIFO.vhd

--- FIFO/FIFO.vhd
+++ FIFO/FIFO.vhd
@@ -10,19 +10,21 @@ entity FIFO is
 
   Port(
     reset : in std_logic;
-    clk : in std_logic;
-    din : in std_logic_vector(FIFO_WIDTH-1 DOWNTO 0);
-    dout : out std_logic_vector(FIFO_WIDTH-1 DOWNTO 0);
-    rd_en : in std_logic;
-    wr_en : in std_logic;
-    full : out std_logic;
-    empty : out std_logic
+    clk   : in std_logic;
+    
+    din   : in std_logic_vector(FIFO_WIDTH-1 DOWNTO 0);
+    dout  : out std_logic_vector(FIFO_WIDTH-1 DOWNTO 0); --parola in uscita
+    
+    rd_en : in std_logic; --quando espellere
+    wr_en : in std_logic; --quando memorizzare
+    full  : out std_logic; --quando memoria piena ( non posso memorizzare)
+    empty : out std_logic  --quando memoria vuota ( non posso espellere ) 
     );
 
 end FIFO;
 
-architecture Behavioral of FIFO
-
+architecture Behavioral of FIFO is
+  
 begin
 
 end Behavioral;
