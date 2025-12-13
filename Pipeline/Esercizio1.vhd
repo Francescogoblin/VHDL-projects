4338ce0e741225319e56e528ce5d5dbd438782f9
Francesco Donini <166855620+Francescogoblin@users.noreply.github.com>
2025-12-13 10:37:13 +0100

    Update Esercizio1.vhd

--- Pipeline/Esercizio1.vhd
+++ Pipeline/Esercizio1.vhd
@@ -1,7 +1,8 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
-entity esercizio1 is
+entity Esercizio1 is
   Port(
     input_a : in std_logic_vector ( 31 DOWNTO 0);
     input_b : in std_logic_vector ( 31 DOWNTO 0);
@@ -13,7 +14,8 @@ entity esercizio1 is
 end esercizio1;
 -----------------------------------------------------------------------------------------------
 -----------------------------------------------------------------------------------------------
-  Architecture Behavioral of esercizio1 is 
+  Architecture Behavioral of Esercizio1 is 
+  
     component multiplier is
 	Port (
 	input_a			: in unsigned(31 DOWNTO 0);
@@ -27,27 +29,27 @@ end esercizio1;
 	input_b			: in unsigned(31 DOWNTO 0);
 	result			: out unsigned(31 DOWNTO 0)
 	);
-    end adder;
+    end component;
 
-    signal n1 : std_logic_vector(31 DOWNTO 0);
-    signal n2 : std_logic_vector(31 DOWNTO 0);
+    signal n1 : unsigned(31 DOWNTO 0);
+    signal n2 : unsigned(31 DOWNTO 0);
 
     begin
 
     MULTIPLIER_INST : multiplier
       Port map ( 
-        input_a <= input_a;
-	      input_b	<= input_b;
-	      result	<= n1;
+          input_a   => unsigned(input_a),
+	      input_b	=> unsigned(input_b),
+	      result	=> n1
       );
       
     ADDER_INST : adder
       Port map ( 
-        input_a <= input_c;
-	      input_b	<= n1;	
-	      result	<= n2
+          input_a   => unsigned(input_c),
+	      input_b	=> n1,
+	      result	=> n2
       );
 
-      result <= n2;
+      result <= std_logic_vector(n2);
       
  end Behavioral;   
