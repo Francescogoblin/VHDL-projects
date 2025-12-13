d7dedc6f8a30c5faf75cc952e1f2a4b2ffb12fc7
Francesco Donini <166855620+Francescogoblin@users.noreply.github.com>
2025-12-13 12:03:35 +0100

    Rename entity and update signal types in VHDL

--- Pipeline/Esercizio2.vhd
+++ Pipeline/Esercizio2.vhd
@@ -1,20 +1,22 @@
+
 library IEEE;
 use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
-entity esercizio2 is
+entity Esercizio1 is
   Port(
     input_a : in std_logic_vector ( 31 DOWNTO 0);
     input_b : in std_logic_vector ( 31 DOWNTO 0);
     input_c : in std_logic_vector ( 31 DOWNTO 0);
     clk     : in std_logic;
-    reset   : in std_logic;
 
     result : out std_logic_vector ( 31 DOWNTO 0)
   );
-end esercizio2;
+end Esercizio1;
 -----------------------------------------------------------------------------------------------
 -----------------------------------------------------------------------------------------------
-  Architecture Behavioral of esercizio1 is 
+  Architecture Behavioral of Esercizio1 is 
+  
     component multiplier is
 	Port (
 	input_a			: in unsigned(31 DOWNTO 0);
@@ -28,29 +30,28 @@ end esercizio2;
 	input_b			: in unsigned(31 DOWNTO 0);
 	result			: out unsigned(31 DOWNTO 0)
 	);
-    end adder;
+    end component;
 
-    signal n1 : std_logic_vector(31 DOWNTO 0);
-    signal n2 : std_logic_vector(31 DOWNTO 0);
-    signal Q  : std_logic_vector(31 DOWNTO 0);
+    signal n1 : unsigned(31 DOWNTO 0);
+    signal n2 : unsigned(31 DOWNTO 0);
 
     begin
 
     MULTIPLIER_INST : multiplier
       Port map ( 
-        input_a <= input_a;
-	      input_b	<= input_b;
-	      result	<= n1;
+          input_a   => unsigned(input_a),
+	      input_b	=> unsigned(input_b),
+	      result	=> n1
       );
       
     ADDER_INST : adder
       Port map ( 
-        input_a <= input_c;
-	      input_b	<= n1;	
-	      result	<= n2
+          input_a   => unsigned(input_c),
+	      input_b	=> n1,
+	      result	=> n2
       );
 
-      result <= Q;
+      result <= std_logic_vector(n2);
 
     process ( clk , reset ) 
       begin
