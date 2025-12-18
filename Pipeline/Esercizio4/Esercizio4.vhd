1bb7191001c2298c0d7488867cded0248e93cd72
Francesco Donini <166855620+Francescogoblin@users.noreply.github.com>
2025-12-18 10:00:56 +0100

    Update Esercizio4.vhd

--- Pipeline/Esercizio4/Esercizio4.vhd
+++ Pipeline/Esercizio4/Esercizio4.vhd
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
-entity Esercizio3 is
+entity Esercizio4 is
   Port(
     input_a : in std_logic_vector ( 31 DOWNTO 0);
     input_b : in std_logic_vector ( 31 DOWNTO 0);
@@ -11,7 +11,7 @@ entity Esercizio3 is
 
     result : out std_logic_vector ( 31 DOWNTO 0)
   );
-end Esercizio3;
+end Esercizio4;
 -----------------------------------------------------------------------------------------------
 -----------------------------------------------------------------------------------------------
   Architecture Behavioral of Esercizio3 is 
@@ -38,31 +38,7 @@ end Esercizio3;
 	
     begin
 
-    MULTIPLIER_INST : multiplier
-      Port map ( 
-          input_a   => unsigned(input_a),
-	      input_b	=> unsigned(input_b),
-	      result	=> n1
-      );
-      
-    ADDER_INST : adder
-      Port map ( 
-          input_a   => n4,
-	      input_b	=> n3,
-	      result	=> n2
-      );
     
-
-    process ( clk , reset ) 
-      begin
-        if reset = '1' then
-          result <= (Others => '0');
-        elsif rising_edge(clk) then
-          result<=std_logic_vector(n2);
-		  n3<=n2;
-		  n4<=unsigned(input_c);
-        end if;
-      end process;
         
  end Behavioral;   
     
