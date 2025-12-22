cebbe592f6811f03ba69f0ad738c601334ca5a08
Francesco Donini <166855620+Francescogoblin@users.noreply.github.com>
2025-12-22 12:22:59 +0100

    Update Esercizio3.vhd

--- Pipeline/Esercizio3/Esercizio3.vhd
+++ Pipeline/Esercizio3/Esercizio3.vhd
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
--- andiamo a spezzare il processo combinatorio più lungo, così che il tempo di propagazione totale è 15ns
+-- andiamo a spezzare il processo combinatorio più lungo, così che il tempo di propagazione totale è 15ns. La latenza del sistema è 2*20ns . Il throughput dipende , come al solito , dal clock
 
 entity Esercizio3 is
   Port(
