686f89245a49ad0f598372293e5b9794123d2bf9
Francesco Donini <166855620+Francescogoblin@users.noreply.github.com>
2025-12-22 11:48:02 +0100

    Update Esercizio2.vhd

--- Pipeline/Esercizio2/Esercizio2.vhd
+++ Pipeline/Esercizio2/Esercizio2.vhd
@@ -3,20 +3,22 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
-entity Esercizio2 is
-  Port(
-    input_a : in std_logic_vector ( 31 DOWNTO 0);
-    input_b : in std_logic_vector ( 31 DOWNTO 0);
-    input_c : in std_logic_vector ( 31 DOWNTO 0);
-    clk     : in std_logic;
-	reset 	: in std_logic;
+-- Tpd = 22ns
 
-    result : out std_logic_vector ( 31 DOWNTO 0)
+entity Esercizio1 is
+  Port(
+    input_a : in std_logic_vector ( 31 DOWNTO 0) ;
+    input_b : in std_logic_vector ( 31 DOWNTO 0) ;
+    input_c : in std_logic_vector ( 31 DOWNTO 0) ;
+    clk     : in std_logic;
+    reset	: in std_logic;
+	  
+    result  : out std_logic_vector ( 31 DOWNTO 0)
   );
-end Esercizio2;
+end Esercizio1;
 -----------------------------------------------------------------------------------------------
 -----------------------------------------------------------------------------------------------
-  Architecture Behavioral of Esercizio2 is 
+  Architecture Behavioral of Esercizio1 is 
   
     component multiplier is
 	Port (
@@ -25,6 +27,7 @@ end Esercizio2;
 	result			: out unsigned(31 DOWNTO 0)
 	);
     end component;
+    
     component adder is
 	Port (
 	input_a			: in unsigned(31 DOWNTO 0);
@@ -33,8 +36,7 @@ end Esercizio2;
 	);
     end component;
 
-    signal n1 : unsigned(31 DOWNTO 0);
-    signal n2 : unsigned(31 DOWNTO 0);
+    signal out_mul : unsigned(31 DOWNTO 0);
 
     begin
 
@@ -42,26 +44,15 @@ end Esercizio2;
       Port map ( 
           input_a   => unsigned(input_a),
 	      input_b	=> unsigned(input_b),
-	      result	=> n1
+	      result	=> out_mul
       );
       
     ADDER_INST : adder
       Port map ( 
           input_a   => unsigned(input_c),
-	      input_b	=> n1,
-	      result	=> n2
+	      input_b	=> out_mul
+	      std_logic_vector(result)	=> result
       );
-
-      result <= std_logic_vector(n2);
-
-    process ( clk , reset ) 
-      begin
-        if reset = '1' then
-          Q <= (Others => '0');
-        elsif rising_edge(clk) then
-          Q<=n2;
-        end if;
-      end process;
-        
+      
  end Behavioral;   
     
